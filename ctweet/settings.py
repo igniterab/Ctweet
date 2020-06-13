c22d6c622ee803f590b51911c7645c03cd9b5d2a
igniterab <56384941+igniterab@users.noreply.github.com>
2020-06-13 23:36:35 +0530

    changed settings.py added STATIC_ROOT link

--- ctweet/settings.py
+++ ctweet/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
